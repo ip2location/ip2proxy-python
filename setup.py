f8adaa65f851f823c5133f30048f7c1db309d3fd
ip2location <support@ip2location.com>
2018-07-11 15:36:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
-#!/usr/bin/env python
+import setuptools
 
-from distutils.core import setup
-setup(name='IP2Proxy',
-        version='1.0.1',
-        description='Python API for IP2Proxy database',
-        author='IP2Location',
-        author_email='support@ip2location.com',
-        url='http://www.ip2location.com',
-        py_modules=['IP2Proxy'],
-        )
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="IP2Proxy",
+	version="1.0.2",
+	author="IP2Location",
+	author_email="support@ip2location.com",
+	description="Python API for IP2Proxy database. It can be used to query an IP address if it was being used as open proxy, web proxy, VPN anonymizer and TOR exits.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	py_modules=['IP2Proxy'],
+	url="https://github.com/ip2location/ip2proxy-python",
+	packages=setuptools.find_packages(),
+	classifiers=(
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	),
+)
