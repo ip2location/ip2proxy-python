c83a53c34b767c09db4f603e1844c7699908e7f9
IP2Location <support@ip2location.com>
2020-08-11 15:44:05 +0800

    Update the code.

--- lookup.py
+++ lookup.py
@@ -1,8 +1,7 @@
 import os, IP2Proxy, sys, ipaddress
 
 # database = IP2Location.IP2Location(os.path.join("data", "IPV6-COUNTRY.BIN"), "SHARED_MEMORY")
-# database = IP2Location.IP2Location(os.path.join("data", "IPV6-COUNTRY.BIN"))
-database = IP2Proxy.IP2Proxy("C:\\OoiKaiWen\\work\\code\\perl\\ip2proxy\\databases\\IP2PROXY-IP-PROXYTYPE-COUNTRY-REGION-CITY-ISP-DOMAIN-USAGETYPE-ASN-LASTSEEN-THREAT-RESIDENTIAL.BIN")
+database = IP2Location.IP2Location(os.path.join("data", "IPV6-COUNTRY.BIN")))
 
 try:
     ip = sys.argv[1]
